9eb69f7a6d769ac8cd87697599c46dfb45a169ec
david.rusycki <david.rusycki@unidavi.edu.br>
2021-09-11 00:33:52 -0300

    adicionado método para suportar também um json como parametro.

--- AutoSelectionHtml/auto_select.js
+++ AutoSelectionHtml/auto_select.js
@@ -1,7 +1,6 @@
-
 /**
  * Classe que contém as funções para criar dinamicamente um elemento "<select>" do html.
- * @ideia A ideia por trás do projeto é passar o id e um array de elementos para serem adicionados em um elemento select do html.
+ * @ideia A ideia por trás do projeto é passar o id e um array/json de elementos para serem adicionados em um elemento select do html.
  */
 var SelectionJs = (function() {
 
@@ -11,15 +10,24 @@ var SelectionJs = (function() {
     var criaSelectFunctionArray = (idElement, aOptions) => {
         oSelection = getElement(idElement);
         aOptions.map((valor, indice) => {
-            var  oOption = montaOption(valor, indice);
-            oSelection.appendChild(oOption);
+            oSelection.appendChild(montaOption(valor, indice));
         });
     };
 
     /**
+     * Realiza o processamento necessário para definir os valores e chaves do Json no select.
+     */
+    var criaSelectionFunctionJson = (idElement, oOptions) => {
+        oSelection = getElement(idElement);
+        for (sChave in oOptions) {
+            oSelection.appendChild(montaOption(oOptions[sChave], sChave));
+        };
+    }
+
+    /**
      * Retorna o elemento HTML do respectivo id.
      * @param {String} idElement - String com o id do elemento HTML.
-     * @returns HTMLELEMENT
+     * @returns HTML ELEMENT
      */
     var getElement = (idElement) => {
         return document.getElementById(idElement);
@@ -28,7 +36,7 @@ var SelectionJs = (function() {
     /**
      * Monta um novo elemento option de acordo com os parâmetros.
      * @param {Mixed} valor - Valor de determinada posição do array.
-     * @param {Mixed} indice - Indice do array que está sendo percorrido.
+     * @param {Mixed} indice - Indice do array/json que está sendo percorrido.
      * @returns Option Element
      */
     var montaOption = (valor, indice) => {
@@ -50,11 +58,21 @@ var SelectionJs = (function() {
 
         /**
          * Adiciona no select os itens do array.
+         * @require para adicionar os valores de um array o array deve ter indices numericos. Caso contrário deve-se usar o método criaSelectFromJson
          * @param {String} idElement - Id do elemento <select> que deseja adicionar os valores do array.
          * @param {Array} aOptions - Array onde o indice será definido como value e o valor em si será o text.
          */
         criaSelectFromArray : (idElement, aOptions) => {
             criaSelectFunctionArray(idElement, aOptions);
+        },
+
+        /**
+         * Adiciona no select os itens do Json
+         * @param {String} idElement - Id do elemento <select> que deseja adicionar os valores.
+         * @param {Json} oOptions - Json onde as chaves serão definidas como value e o valor em si será o text.
+         */
+        criaSelectionFromJson : (idElement, oOptions) => {
+            criaSelectionFunctionJson(idElement, oOptions);
         }
 
     };
